31e5bd6d018374d2e229889e024b9f58dcea8411
sanjeevtalluri <sanjeevtalluri@gmail.com>
2024-09-03 13:18:20 +0530

    latest commit

--- src/main/java/com/youtube/ecommerce/configuration/ApplicationDevConfig.java
+++ src/main/java/com/youtube/ecommerce/configuration/ApplicationDevConfig.java
@@ -15,7 +15,7 @@ public class ApplicationDevConfig {
     public DataSource dataSource() {
         return DataSourceBuilder
                 .create()
-                .url("jdbc:mysql://ecommerce.c9s2qgooyrj4.eu-north-1.rds.amazonaws.com:3306/ecommerce")
+                .url("jdbc:mysql://localhost:3306/ecommerce")
                 .username("mysqldbe")
                 .password("mysqldbe")
                 .build();
